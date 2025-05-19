555230553c0ec12695c6a65a249db3302b8f93a1
user <khg011126@gmail.com>
2025-05-19 15:22:17 +0900

    first commit

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,27 @@
+package com.shop.repository;
+
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.List;
+
+// 첫번째 매개변수 : 엔티티 타입 , 두번째 매개변수 : 기본키 타입
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail); //  itemDetail 타입
+
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail); //  itemDetail 타입
+}
